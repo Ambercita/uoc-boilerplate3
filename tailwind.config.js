a498fb5628878c086eb2b83654e3c76df37a6617
Jia Wang <amber.jiawang@gmail.com>
2025-01-18 00:56:53 +0100

    header modified, index tmb, im in news

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,13 +5,6 @@ module.exports = {
     mono: ['Monaco', 'monospace'],
     serif: ['ui-serif', 'Georgia'],
   },
-  colors: {
-    'color-primary': '#D90429',
-    'color-secondary': '#EF233C',
-    'color-background': '#EDF2F4',
-    'color-quaternary': '#8D99AE',
-    'color-quinary': '#2B2D42',
-  },
   theme: {
     extend: {},
   },
