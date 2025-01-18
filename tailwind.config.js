ad60130f5615b59a86f02611c0329e5c53ae698b
Jia Wang <amber.jiawang@gmail.com>
2025-01-18 19:57:35 +0100

    personalizacion color puesto bien

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js}"],
-  fontFamily: {
-    mono: ['Monaco', 'monospace'],
-    serif: ['ui-serif', 'Georgia'],
-  },
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        mono: ["Monaco", "monospace"],
+        serif: ["ui-serif", "Georgia"],
+      },
+      colors: {
+        primary: "#dc2626",
+        secondary: "#b91c1c",
+        text: "#1f2937",
+        subtext: "#374151",
+        fondo: "#e2e8f0",
+        border: "#fca5a5",
+      },
+    },
   },
   plugins: [],
 };
